710360224e800e03250325361425a6690b81cc13
Pratik Daherwal <38158708+pratikdaherwal@users.noreply.github.com>
2023-02-15 15:27:55 +0000

    initial commit

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,36 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int binarySearch(vector<int> &arr, int ele){
+int binarySearch(vector<int> &arr, int ele) {
     int n = arr.size();
     int low = 0;
-    int high = n-1;
-    for (int i=0; i<n;i++){
-        int mid = (high+low)/2;
+    int high = n - 1;
 
-        while (low < high){
-            if (arr[mid] == ele){
-                return mid;
-            }
-            else if(arr[mid] < ele){
-                mid += 1;
-            }
-            else{
-                mid -= 1;
-            }
+    while (low <= high) {
+        int mid = (high + low) / 2;
+
+        if (arr[mid] == ele) {
+            return mid;
+        }
+        else if (ele < arr[mid]) {
+            high -= 1;
+        }
+        else {
+            low += 1;
         }
     }
     return -1;
 }
 
+
+
 int main(){
     vector<int> arr {1,2,3,4,5,6,7,8,9,0};
 
-    for (auto i=arr.begin(); i<arr.end();i++){
-        cout<<*i<<" ";
-    }
+    // for (auto i=arr.begin(); i<arr.end();i++){
+    //     cout<<*i<<" ";
+    // }
 
-    cout<<"found at index "<<binarySearch(arr, 4)<<endl;
+    cout<<"found at index "<<binarySearch(arr, 1)<<endl<<endl<<endl;
 
     return 0;
 }
